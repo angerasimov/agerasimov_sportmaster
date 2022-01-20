4d1d2f76fad9019b517612ba1522e11af95a3975
Andrey Gerasimov <agerasimov.handh@gmail.com>
2022-01-20 09:22:10 +0300

    Add next code: - next steps (input code - permission location - confirmation of city - tap Profile) - capabilities (udid, auto_grant_permission - comment) - increase TimeUnit.SECONDS in implicityWait from 5 to 15 - add "println" to every step

--- src/test/java/main/BaseClass.kt
+++ src/test/java/main/BaseClass.kt
@@ -25,17 +25,18 @@ class BaseClass {
 
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android") // название платформы
         caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"11") // версия ОС
-        caps.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 5") // имя устройства
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME,"Nokia 1.3") // имя устройства
         caps.setCapability(MobileCapabilityType.NO_RESET,false) // не сбрасывать приложение в 0 перед новым запуском
         caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT,"7200")
         caps.setCapability(MobileCapabilityType.APP,"/Users/andreygerasimov/Downloads/sportmaster-4.0.13.5605_dev_beta.apk") // путь к файлу
         caps.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,"ru.sportmaster.app.handh.dev") // имя пакета
         caps.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"ru.sportmaster.app.presentation.start.StartActivity") // название активити
-        //caps.setCapability(MobileCapabilityType.UDID,"")
+        caps.setCapability(MobileCapabilityType.UDID,"PT89572AA1A60800280")
+        //caps.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, true)
 
         driver = AndroidDriver(url, caps) // установка драйвера и приложения на андроид устройство
 
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS) // будет искать элемент в течение 5 секунд
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS) // будет искать элемент в течение 15 секунд
 
     }
 
@@ -46,13 +47,13 @@ class BaseClass {
 
     @Test
     fun testOne(){
-        TimeUnit.SECONDS.sleep(1)
+        TimeUnit.SECONDS.sleep(2)
 
         try {
             lateinit var element: MobileElement // создаем элемент MobileElement
-            element = driver.findElement(MobileBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageButton\n")) // производим поиск элемента по локатору
+            element = driver.findElement(MobileBy.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageButton")) // производим поиск элемента по локатору
             element.click()
-            println("клик прошел успешно")
+            println("Клик по крестику прошел успешно")
         } catch (e: org.openqa.selenium.NoSuchElementException) {
             println("Элемент не найден, тест продолжается")
         }
@@ -60,12 +61,38 @@ class BaseClass {
         // Ввод текста в поле
         lateinit var element2: MobileElement //создаем элемент MobileElement
         element2 = driver.findElement(MobileBy.id("ru.sportmaster.app.handh.dev:id/editTextPhone")) // поиск элемента по локатору
-        element2.sendKeys("9999999905") // ввод текста в поле
+        element2.sendKeys("9999999969") // ввод текста в поле
+        println("Номер телефона введен")
 
-        //
+        // Клик по кнопке
         lateinit var element3: MobileElement //создаем элемент MobileElement
         element3 = driver.findElement(MobileBy.id("ru.sportmaster.app.handh.dev:id/buttonGetCode")) //поиск элемента по локатору
         element3.click() // клик по кнопке
+        println("Клик по кнопке прошел успешно")
+
+        // Ввод кода
+        lateinit var element4: MobileElement //создаем элемент MobileElement
+        element4 = driver.findElement(MobileBy.id("ru.sportmaster.app.handh.dev:id/pinCodeEditText")) //поиск элемента по локатору
+        element4.sendKeys("1111") //ввод кода в поле
+        println("Код введен")
+
+        // Даем добро на местоположение
+        lateinit var element5: MobileElement //создаем элемент MobileElement
+        element5 = driver.findElement(MobileBy.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")) //поиск элемента по локатору
+        element5.click() //клик по кнопке
+
+        // Подтверждение города
+        lateinit var element6: MobileElement //создаем элемент MobileElement
+        element6 = driver.findElement(MobileBy.id("android:id/button1")) //поиск элемента по локатору
+        element6.click() //клик по кнопке Да
+        println("Подтверждение города прошло успешно")
+
+        // Переход по табу в профиль
+        lateinit var element7: MobileElement //создаем элемент MobileElement
+        element7 = driver.findElement(MobileBy.id("ru.sportmaster.app.handh.dev:id/profile_graph")) //поиск элемента по локатору
+        element7.click() //тап по табу Профиль
+        println("Переход по табу в профиль прошел успешно")
+
 
         TimeUnit.SECONDS.sleep(5)
     }
